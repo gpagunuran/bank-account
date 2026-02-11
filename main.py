35cf02d9a90b8e0ce5b248f92d4e2328038c6e9c
gpagunuran <gabe.pagunuran@gmail.com>
2026-02-10 22:50:59 -0500

    fix: change account numbers to string

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import checking_account as check
 #Create a scenario (i.e a user opens a checking account and withdraws $x) and call proper methods to illustrate the scenario.
 
 #Create 2 savings accounts and checking accounts instances
-gp_savings = save.savings_account(01, 001)
-gp_checking = check.checking_account(01, 100)
-el_savings = save.savings_account(02, 002)
-el_checking = check.checking_account(02, 200)
+gp_savings = save.savings_account('01', '001')
+gp_checking = check.checking_account('01', '100')
+el_savings = save.savings_account('02', '002')
+el_checking = check.checking_account('02', '200')
 
 
 ##
