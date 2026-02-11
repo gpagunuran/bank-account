cd89d1c597e18a00014f2e1d7b21e60a6cb2543f
ethanrlee1 <ethanrlee1@gmail.com>
2026-02-11 14:57:59 -0500

    feat: created code for Savings_account

--- savings_account.py
+++ savings_account.py
@@ -1,6 +1,8 @@
+from BankAccount import BankAccount
+
 class SavingsAccount(BankAccount):
-    def __init__(self, account_number, routing_number, balance=0, interest_rate=0.02):
-        super().__init__(account_number, routing_number, balance)
+    def __init__(self, account_number, balance=0, interest_rate=0.02):
+        super().__init__(account_number, balance)
         self.interest_rate = interest_rate
 
     def apply_interest(self):
